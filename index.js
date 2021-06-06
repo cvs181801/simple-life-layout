c665d1ce158f3eba50d85ee615d55267a1cc4936
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-06 13:59:01 -0700

    adding js

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-//keep the current page selected using event listeners.
+// //keep the current page selected using event listeners.
 
-//first, grab the ID's for each navbar selector.
-const home = document.getElementById("home-selector");
-const aboutMe = document.getElementById("about-me-selector");
-const recentPosts = document.getElementById("recent-posts-selector");
+// //first, grab the ID's for each navbar selector.
+// const home = document.getElementById("home-selector");
+// const aboutMe = document.getElementById("about-me-selector");
+// const recentPosts = document.getElementById("recent-posts-selector");
 
 
-//highlight home nav selector
-home.addEventListener("click", event => {
-    aboutMe.classList.remove("current-page");
-    recentPosts.classList.remove("current-page");
-    home.classList.add("current-page");
+// //highlight home nav selector
+// home.addEventListener("click", event => {
+//     aboutMe.classList.remove("current-page");
+//     recentPosts.classList.remove("current-page");
+//     home.classList.add("current-page");
     
-});
+// });
    
-//highlight about me nav selector    
+// //highlight about me nav selector    
 
 
-aboutMe.addEventListener("click", event => {
-    home.classList.remove("current-page");
-    recentPosts.classList.remove("current-page");
-    aboutMe.classList.add("current-page");
-});
+// aboutMe.addEventListener("click", event => {
+//     home.classList.remove("current-page");
+//     recentPosts.classList.remove("current-page");
+//     aboutMe.classList.add("current-page");
+// });
 
-//highlight recent posts nav selector
+// //highlight recent posts nav selector
 
 
-recentPosts.addEventListener("click", event => {
-    home.classList.remove("current-page");
-    aboutMe.classList.remove("current-page");
-    recentPosts.classList.add("current-page");
-});
+// recentPosts.addEventListener("click", event => {
+//     home.classList.remove("current-page");
+//     aboutMe.classList.remove("current-page");
+//     recentPosts.classList.add("current-page");
+// });
         
-        
+//this solution works for other kinds of elements like div's but not working for our navbar.  I think it's because the li's in the nav bar form a node list which is not an array but similar
+
+//try again
+
+const navigationBar = Array.from(document.querySelectorAll('#navbar>ul>li'));
