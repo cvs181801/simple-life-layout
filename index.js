5242277984121f8dc95545158fb9ac331db7db04
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-06 13:25:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-//keep the current page selected 
+//keep the current page selected using event listeners.
 
-const currentPage = document.querySelector("nav-selector");
+//first, grab the ID's for each navbar selector.
+const home = document.getElementById("home-selector");
+const aboutMe = document.getElementById("about-me-selector");
+const recentPosts = document.getElementById("recent-posts-selector");
 
-currentPage.addEventListener("click", event => {
-    currentPage.addClass("current-page");
+
+//highlight home nav selector
+home.addEventListener("click", event => {
+    aboutMe.classList.remove("current-page");
+    recentPosts.classList.remove("current-page");
+    home.classList.add("current-page");
+    
 });
+   
+//highlight about me nav selector    
+
+
+aboutMe.addEventListener("click", event => {
+    home.classList.remove("current-page");
+    recentPosts.classList.remove("current-page");
+    aboutMe.classList.add("current-page");
+});
+
+//highlight recent posts nav selector
+
+
+recentPosts.addEventListener("click", event => {
+    home.classList.remove("current-page");
+    aboutMe.classList.remove("current-page");
+    recentPosts.classList.add("current-page");
+});
+        
+        
