a4910ab11de010c7d773b55c7419252a1c36533a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-05 21:39:06 -0700

    add JS

---
+++ index.js
@@ -0,0 +1,6 @@
+//keep the current page selected 
+
+document.getElementById("nav-selector").addEventListener("click", function() {
+    element.style.backgroundColor = "#FFDEAC";
+    //"1px solid #707070";
+})
